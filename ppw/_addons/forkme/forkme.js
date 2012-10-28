59501b28813740c2d2cfc8e15b239eb3e6618c57
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2012-10-28 21:50:54 -0200

    Adding forkme addon files

---
+++ ppw/_addons/forkme/forkme.js
@@ -0,0 +1,9 @@
+window.PPW.extend("forkme", (function(){
+    
+    return {
+        onsplashscreen: function(){
+            $('#ppw-addons-container').append("<span><a href='http://github.com/braziljs/powerpolygon' target='_blank'>Forme ke on Github</a></span>");
+        }
+    };
+    
+})());
