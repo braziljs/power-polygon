7a36ff14698ec590e00e8c0090e42c238a8000c7
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2012-11-25 18:35:14 -0200

    Changed the address of the project in addons and links

--- ppw/_addons/forkme/forkme.js
+++ ppw/_addons/forkme/forkme.js
@@ -2,7 +2,7 @@ window.PPW.extend("forkme", (function(){
     
     return {
         onsplashscreen: function(){
-            $('#ppw-addons-container').append("<span><a href='http://github.com/braziljs/powerpolygon' target='_blank'>Forme ke on Github</a></span>");
+            $('#ppw-addons-container').append("<span><a href='http://github.com/braziljs/power-polygon' target='_blank'>Forme ke on Github</a></span>");
         }
     };
     
