06b7fa9a9e74997501b948e6f10ad68b030639ea
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2013-01-26 17:32:10 -0200

    Fixed problem with thm-parallax-1

--- ppw/_themes/thm-parallax-1/scripts/default-parallax.js
+++ ppw/_themes/thm-parallax-1/scripts/default-parallax.js
@@ -2,7 +2,7 @@ PPW.addListener('onstart', function(data){
     //$(PPW.getCurrentSlide().el.parentNode).fadeIn();
     
 });
-/*
+
 PPW.addListener('onslidechange', function(data){
     
     var currentSlide= PPW.getCurrentSlide();
@@ -10,15 +10,14 @@ PPW.addListener('onslidechange', function(data){
     $(currentSlide.el).find('.parallax-set').find('.far, .close, .medium').addClass('active');
     
     if(data.current != data.previous){
-        $(currentSlide.el.parentNode).fadeIn(function(){
+        //$(currentSlide.el.parentNode).fadeIn(function(){
             //console.log($(currentSlide.el).find('.parallax-set').find('.far, .close, .medium'))
             
             
-            $(data.previous.el.parentNode).fadeOut();
+            //$(data.previous.el.parentNode).fadeOut();
             $(data.previous.el).find('.parallax-set').find('.far, .close, .medium').removeClass('active');
-        });
+        //});
         
     }
     
 });
-*/
