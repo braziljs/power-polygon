9efff25de3c72ec9aa0e2b428ba9d027fbdc4b8f
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2013-03-05 02:54:05 -0300

    Fixed a few issues to the source of font-awesome; Given back, the $.browser to the new versions of jQuery

--- ppw/ppw.js
+++ ppw/ppw.js
@@ -99,6 +99,46 @@ window.PPW= (function($, _d, console){
 
            };
     })($);
+    
+    /**
+     * Giving back the $.browser to the jquery lib.
+     * 
+     * The jQuery 1.9.1 removed the browser object, so we add it back.
+     */
+    if(!$.browser){
+        (function(){
+            var matched = (function(ua){
+                    ua = ua.toLowerCase();
+
+                    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+                            /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+                            /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+                            /(msie) ([\w.]+)/.exec( ua ) ||
+                            ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+                            [];
+
+                    return {
+                            browser: match[ 1 ] || "",
+                            version: match[ 2 ] || "0"
+                    };
+                })( navigator.userAgent );
+            var browser = {};
+
+            if ( matched.browser ) {
+                    browser[ matched.browser ] = true;
+                    browser.version = matched.version;
+            }
+
+            // Chrome is Webkit, but Webkit is also Safari.
+            if ( browser.chrome ) {
+                    browser.webkit = true;
+            } else if ( browser.webkit ) {
+                    browser.safari = true;
+            }
+            jQuery.browser= $.browser= browser;
+        })();
+    }
+    
     /* END VALIDATING AND SETTING UP */
     
     /**************************************************
