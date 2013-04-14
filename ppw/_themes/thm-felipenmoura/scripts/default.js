c84d876e18e4eb972257d40ddd7d678e5a887039
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2013-04-14 16:44:19 -0300

    Added theme files

---
+++ ppw/_themes/thm-felipenmoura/scripts/default.js
@@ -0,0 +1,16 @@
+
+PPW.addListener('onstart', function(data){
+    //$(PPW.getCurrentSlide().el).stop().fadeIn();
+});
+
+PPW.addListener('onslidechange', function(data){
+    /*
+    var currentSlide= PPW.getCurrentSlide();
+    
+    if(data.current != data.previous){
+        $(data.previous.el).fadeOut(function(){
+            $(PPW.getCurrentSlide().el).fadeIn();
+        });
+    }
+    */
+});
