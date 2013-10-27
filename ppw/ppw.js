72c50585eb5e5fc31d214716fa13fb8e331bbf50
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2013-10-27 03:17:16 -0200

    Applying the new design for the toolbars

--- ppw/ppw.js
+++ ppw/ppw.js
@@ -352,10 +352,13 @@ window.PPW = (function ($, _d, console){
                       </div>',
 
             // the top-left toolbar content
-            toolBar: '<div id="ppw-toolbar-container" tabindex="0" class="ppw-platform {{clickableClass}}">\
-                    <div id="ppw-toolbar" class="ppw-platform {{clickableClass}}">\
-                    </div>\
-                   </div>',
+            toolBar: '<div id="ppw-toolbar-container" tabindex="0" class="ppw-platform ppw-clickable">\
+                        <div id="ppw-toolbar-trigger-btn" class="ppw-clickable"><span></span></div>\
+                        <div id="ppw-toolbar-v" class="ppw-platform {{clickableClass}}">\
+                        <div id="ppw-toolbar-h" class="ppw-platform {{clickableClass}}">\
+                        </div>\
+                        <div id="ppw-toolbar-overlay"></div>\
+                      </div>',
 
             // the settings form content
             settings: "<form id='ppw-settings-form'>\
@@ -1159,13 +1162,6 @@ window.PPW = (function ($, _d, console){
          }
 
         _loadTheme();
-
-        if(!_isInPrintableVersion()){
-            _bindEvents();
-        }else{
-            _triggerEvent('onthemeloaded', _settings.themeSettings);
-            $('#ppw-lock-loading').hide();
-        }
     };
 
     /**
@@ -2114,11 +2110,11 @@ window.PPW = (function ($, _d, console){
         });
 
         // scrolling, used to apply zoom effects
-        if(_settings.zoomOnScroll){
+        /*if(_settings.zoomOnScroll){
             mouseWheelFn= _mouseWheelZoom;
             $d.bind('DOMMouseScroll', mouseWheelFn);
             $d.bind('mousewheel', mouseWheelFn);
-        }
+        }*/
 
         /**
          * Online/Offline events
@@ -2186,6 +2182,11 @@ window.PPW = (function ($, _d, console){
 
             return null;
         };
+
+        /* Toolbar Events */
+        $('#ppw-toolbar-trigger-btn').live('click', function(){
+            $('#ppw-toolbar-container').toggleClass('active');
+        });
 
         /*$b.bind('selectstart', function(evt){
 
@@ -4426,14 +4427,18 @@ window.PPW = (function ($, _d, console){
      * @paran Object The icon data: {id, description, click }
      * @return jQueryObject A reference to the created icon.
      */
-    var _createIcon= function(iconData){
+    var _createIcon= function(iconData, v){
 
-        var toolBar= _getAndCache('#ppw-toolbar'),
+        var toolBar= _getAndCache(v? '#ppw-toolbar-v': '#ppw-toolbar-h'),
+            specialStyle= v? '-webkit-transition-delay: '+iconData.delay+'; '+
+                             '-moz-transition-delay: '+iconData.delay+'; '+
+                             'transition-delay: '+iconData.delay+ '; ': '',
             icon= $('<span href="javascript:void(0);" \
                         class="ppw-icon" \
                         alt="'+iconData.description+'" \
                         title="'+iconData.description+'" \
-                        style="background-image: url('+iconData.image+');"></span>');
+                        style="background-image: url('+iconData.image+');\
+                               '+specialStyle+'"></span>');
         icon.bind('click', iconData.click);
         toolBar.append(icon);
         //alert(toolBar.length)
@@ -4471,7 +4476,6 @@ window.PPW = (function ($, _d, console){
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) isMobile= true})(navigator.userAgent||navigator.vendor||window.opera);
         _conf.isMobile= isMobile;
 
-
         _createSplashScreen();
 
         if(!_isInPrintableVersion()){
@@ -4479,6 +4483,13 @@ window.PPW = (function ($, _d, console){
             _goToSlide(_conf.currentSlide);
         }else{
             _goToSlide(0);
+        }
+
+        if(!_isInPrintableVersion()){
+            _bindEvents();
+        }else{
+            _triggerEvent('onthemeloaded', _settings.themeSettings);
+            $('#ppw-lock-loading').hide();
         }
 
         _initRemoteService();
