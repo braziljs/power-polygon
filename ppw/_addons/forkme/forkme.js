f43cda46c4dc9899b62de8d7a91a4b85b0667da4
Felipe N. Moura <felipenmoura@gmail.com>
2014-12-03 21:56:12 -0200

    [-] Removed all previous files

--- ppw/_addons/forkme/forkme.js
+++
@@ -1,9 +0,0 @@
-window.PPW.extend("forkme", (function(){
-    
-    return {
-        onsplashscreen: function(){
-            $('#ppw-addons-container').append("<span><a href='http://github.com/braziljs/power-polygon' target='_blank'>Fork me on Github</a></span>");
-        }
-    };
-    
-})());
--- ppw/_themes/trans-3d-5/scripts/default.js
+++
@@ -1,3 +0,0 @@
-PPW.addListener('onslidechange', function(data){
-    //$('.ppw-slide-container:not(.ppw-active-slide-element-container)').hide();
-});
